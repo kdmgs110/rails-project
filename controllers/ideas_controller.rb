848b469ba734c3dc25909b8140b75c4be7b58511
Dai <kdmgs110@gmail.com>
2016-08-07 22:55:10 +0000

    removed index method from ideas_controller.rb and fixed validation fron ideas.rb

--- controllers/ideas_controller.rb
+++ controllers/ideas_controller.rb
@@ -1,8 +1,4 @@
 class IdeasController < ApplicationController
-    def index
-        @ideas = Idea.all
-    end
-    
     def create
         @idea = Idea.create(idea_params)
         redirect_to root_path
